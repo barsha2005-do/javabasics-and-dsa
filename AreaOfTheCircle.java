ac6c2aac1e38366351a080d0a326228ab1a46a81
Barsha Priyadarsini <barshap674@gmail.com>
2025-09-03 18:28:33 +0530

    JAva Basics and dsa concepts which will help you to write your own code and also use you can solve more problems using this file

---
+++ AreaOfTheCircle.java
@@ -0,0 +1,44 @@
+//Enter import java.util.*;
+public class AreaOfTheCircle{
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // float r = sc.nextFloat(); // Initializing r with a value
+        // float area = 3.14f*r*r;
+        // System.out.println("Area of the circle: " + area);
+
+        // int val = 1, sum =0, n=10; // Initializing n with a value
+        // while(val<=n){
+        //     System.out.println(sum);
+        //     sum = sum +val;
+        //     val = val+1;
+
+        // int div=2, n=30;
+        // while (div<n) { 
+        //     if (n%div == 0){
+        //         System.out.println("Not a Prime number");
+        //     }
+        //     else{
+        //         div = div +1;
+        //     
+        // int a = 20, b = 30;
+        // if(a>b){
+        //     System.out.println("a is the greatest one");
+
+        // }
+        // else{
+        //     System.out.println("b is the greatest one "); }
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int product = a*b;
+        // System.out.println(product);
+
+    }
+}
