8c4622bb2b6b906b29c28aa04e47ea143cd42b95
Barsha Priyadarsini <barshap674@gmail.com>
2025-09-03 18:41:27 +0530

    i update the files

--- BasicsOfJava.java
+++ BasicsOfJava.java
@@ -1,16 +1,23 @@
 //Enter import java.util.*;
-public class AreaOfTheCircle{
+public class BasicsOfJava{
     public static void main(String[] args) {
+        //Area Of The Circle using Scanner Class.
+
         // Scanner sc = new Scanner(System.in);
         // float r = sc.nextFloat(); // Initializing r with a value
         // float area = 3.14f*r*r;
         // System.out.println("Area of the circle: " + area);
+        
+
+        //sum of first natural numbers.
 
         // int val = 1, sum =0, n=10; // Initializing n with a value
         // while(val<=n){
         //     System.out.println(sum);
         //     sum = sum +val;
         //     val = val+1;
+
+        //Prime Numbers
 
         // int div=2, n=30;
         // while (div<n) { 
