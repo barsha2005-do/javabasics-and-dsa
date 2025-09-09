0f58e0e562a281795d8069eb253cb2c673c58dcf
Barsha Priyadarsini <barshap674@gmail.com>
2025-09-09 22:25:05 +0530

    updated

---
+++ Conditional Staements/Practiceques.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+// public class Practiceques{
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner (System.in);
+//         int num = sc.nextInt();
+//         if(num>0){
+//             System.out.println("Positive");
+//         }
+//         else{
+//             System.out.println("negative");
+//         }
+//     }
+// }
+
