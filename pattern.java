28adf20cda7400cd6ed729afc4a385c4530f4861
barsha2005-do <barshap674@gmail.com>
2026-07-10 05:25:31 +0530

    Refactor pattern printing logic to improve pyramid structure and adjust spacing

--- pattern.java
+++ pattern.java
@@ -44,12 +44,23 @@ public class pattern {
         //     System.out.println();
         // }
 
-        int n= 5;
+        // int n= 5;
+        // for(int row=1; row<=n; row++){
+        //     for(int col=1; col<=n-row; col++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int col=1; col<= 2*row-1; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int n = 4;
         for(int row=1; row<=n; row++){
-            for(int col=1; col<=n-row; col++){
+            for(int col=1; col<=row-1; col++){
                 System.out.print("  ");
             }
-            for(int col=1; col<= 2*row-1; col++){
+            for(int col=1; col<= 2*n-2*row+1; col++){
                 System.out.print("* ");
             }
             System.out.println();
