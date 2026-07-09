d793364a2881f6e21653625c671e3f38933059c5
barsha2005-do <barshap674@gmail.com>
2026-07-09 12:44:36 +0530

    Refactor pattern class to implement right-aligned triangle printing

--- pattern.java
+++ pattern.java
@@ -16,12 +16,24 @@ public class pattern {
         //     System.out.println();
         // }
 
-int n= 5;
-for(int row=1; row<=n; row++){
-    for(int col=1; col<=row; col++){
-        System.out.print("* ");
-    }
-    System.out.println();
-}
+// int n= 5;
+// for(int row=1; row<=n; row++){
+//     for(int col=1; col<=row; col++){
+//         System.out.print("* ");
+//     }
+//     System.out.println();
+// }
+
+           int n = 5;
+           for(int row=1; row<=n; row++){
+            for(int col=1; col<=n-row; col++){
+                System.out.print(" ");
+            }
+            for(int col=1; col<=n; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+           }
+
     }
 }
