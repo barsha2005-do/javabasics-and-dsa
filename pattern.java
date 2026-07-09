0c17758c5e38377c74e28a95b4e0d6b227b050d1
barsha2005-do <barshap674@gmail.com>
2026-07-09 12:33:39 +0530

    Add pattern class implementation for printing a right-angled triangle

---
+++ pattern.java
@@ -0,0 +1,27 @@
+public class pattern {
+    public static void main(String[] args){
+        // int n = 4;
+        // for(int row=1; row<=n; row++){
+        //     for(int col=1; col<=n; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 3;
+        // for (int row=1; row<=n; row++){
+        //     for (int col=1; col<=5; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+int n= 5;
+for(int row=1; row<=n; row++){
+    for(int col=1; col<=row; col++){
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+    }
+}
