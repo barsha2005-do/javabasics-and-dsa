2d29c6bdb7e722038225b219826c35333a344edd
barsha2005-do <barshap674@gmail.com>
2026-07-10 04:56:05 +0530

    Add implementation for printing a right-aligned triangle pattern

--- pattern.java
+++ pattern.java
@@ -36,7 +36,13 @@ public class pattern {
         //    }
 
 
-        
+        int n = 5;
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=n-row+1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
