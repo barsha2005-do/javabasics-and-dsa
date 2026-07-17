48d49a44fc441749178f929adc4ca95555f1c052
barsha2005-do <barshap674@gmail.com>
2026-07-17 23:25:47 +0530

    Add initial implementation of Methods class with tableOfTwo method

---
+++ Methods/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+
+   public static void tableOfTwo() {
+        for (int i = 1; i <= 10; i++) {
+            int ans = i * 2;
+            System.out.println("->" + ans);
+        }
+    }
+
+    public static void main(String[] args) {
+        tableOfTwo();
+    }
+}
