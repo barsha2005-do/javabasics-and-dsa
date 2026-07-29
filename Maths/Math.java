a4dc1e3877ddc55807b3ceaf3727c2c862f2769e
barsha2005-do <barshap674@gmail.com>
2026-07-29 19:23:49 +0530

    Refactor Math class to implement digit summation functionality

--- Maths/Math.java
+++ Maths/Math.java
@@ -1,15 +1,47 @@
 public class Math {
 
-    public static void PrintDigits(int num){
-       while(num!=0){
-          int digit = num%10;
-          System.out.println(digit);
-          num = num/10;
 
-       }
+    public static int SumOfDigits(int num){
+    int sum = 0;
+    while (num!=0) {
+        int digit = num%10;
+        sum = sum*10 +digit;
+        num = num/10;
     }
-   public static void main(String[] args) {
-     int ans = 52134;
-     PrintDigits(ans);
-   }    
+    return sum;
+}
+    public static void main(String[] args) {
+        int num = 5321;
+        int ans  = SumOfDigits(num);
+        System.out.println(ans);
+    }
+    
+
+// public static int CountDigits(int num){
+//     int count = 0;
+//     while (num!=0) {
+//         int digit = num%10;
+//         count++;
+//         num = num/10;
+//     }
+//     return count;
+// }
+//     public static void main(String[] args) {
+//         int num = 5321;
+//         int ans  = CountDigits(num);
+//         System.out.println(ans);
+//     }
+
+//     public static void PrintDigits(int num){
+//        while(num!=0){
+//           int digit = num%10;
+//           System.out.println(digit);
+//           num = num/10;
+
+//        }
+//     }
+//    public static void main(String[] args) {
+//      int ans = 52134;
+//      PrintDigits(ans);
+//    }    
 }
