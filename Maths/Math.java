42764e8e3708cb2edb771afdceabdb236b862a31
barsha2005-do <barshap674@gmail.com>
2026-08-12 11:42:22 +0530

    Update Math class to implement prime number checking functionality

--- Maths/Math.java
+++ Maths/Math.java
@@ -1,20 +1,47 @@
 public class Math {
 
 
-    public static int SumOfDigits(int num){
-    int sum = 0;
-    while (num!=0) {
-        int digit = num%10;
-        sum = sum*10 +digit;
-        num = num/10;
-    }
-    return sum;
-}
     public static void main(String[] args) {
-        int num = 5321;
-        int ans  = SumOfDigits(num);
-        System.out.println(ans);
+        int num = 7;
+        boolean isprime = true;
+        if(num%2==0){
+            isprime = false;
+        }
+        else{
+            isprime = true;
+        }
+        System.out.println(isprime);
+        }
     }
+
+
+//     public static int SumOfDigits(int num){
+//     int sum = 0;
+//     while (num!=0) {
+//         int digit = num%10;
+//         sum = sum*10 +digit;
+//         num = num/10;
+//     }
+//     return sum;
+// }
+
+// public static boolean Palindrom(int num){
+//     int pal = num;
+//     int revNum = SumOfDigits(num);
+//     if(revNum==pal){
+//         System.out.println("it's palindrom");
+//         return true;
+//     }
+//     else{
+//         System.out.println("not a palindrom");
+//         return false;
+//     }
+// }
+//     public static void main(String[] args) {
+        
+//         boolean ans  = Palindrom(1221);
+//         System.out.println(ans);
+//     }
     
 
 // public static int CountDigits(int num){
